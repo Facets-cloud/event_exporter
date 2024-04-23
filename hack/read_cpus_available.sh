ba9fadf2972bd8321cdc082e73379b631a50de70
adityaprajapati <aditya.prajapati@facets.cloud>
2024-04-23 18:27:34 +0530

    reverted read_cpus_available.sh

--- hack/read_cpus_available.sh
+++ hack/read_cpus_available.sh
@@ -1,38 +1,22 @@
 #!/bin/bash
-#set -e
-#
-#CPUS_AVAILABLE=1
-#
-#case "$(uname -s)" in
-#Darwin)
-#    CPUS_AVAILABLE=$(sysctl -n machdep.cpu.core_count)
-#    ;;
-#Linux)
-#    CFS_QUOTA=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
-#    if [ $CFS_QUOTA -ge 100000 ]; then
-#    CPUS_AVAILABLE=$(expr ${CFS_QUOTA} / 100 / 1000)
-#    fi
-#    ;;
-#*)
-#    # Unsupported host OS. Must be Linux or Mac OS X.
-#    ;;
-#esac
-#
-#echo ${CPUS_AVAILABLE}
-
-
-
 
 set -e
 
 CPUS_AVAILABLE=1
 
-if [ "$(uname -s)" == "Linux" ]; then
+case "$(uname -s)" in
+Darwin)
+    CPUS_AVAILABLE=$(sysctl -n machdep.cpu.core_count)
+    ;;
+Linux)
     CFS_QUOTA=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
-    if [ "$CFS_QUOTA" -ge 100000 ]; then
-    # shellcheck disable=SC2003
-    CPUS_AVAILABLE=$(expr "${CFS_QUOTA}" / 100 / 1000)
+    if [ $CFS_QUOTA -ge 100000 ]; then
+    CPUS_AVAILABLE=$(expr ${CFS_QUOTA} / 100 / 1000)
     fi
-fi
+    ;;
+*)
+    # Unsupported host OS. Must be Linux or Mac OS X.
+    ;;
+esac
 
-echo "${CPUS_AVAILABLE}"
+echo ${CPUS_AVAILABLE}

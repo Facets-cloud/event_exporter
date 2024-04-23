b836e5d3a0b49541c555a97b3554df645a6975c1
adityaprajapati <aditya.prajapati@facets.cloud>
2024-04-23 15:57:33 +0530

    updated make file to only handle linux cpu

--- hack/read_cpus_available.sh
+++ hack/read_cpus_available.sh
@@ -1,22 +1,37 @@
+#
+#set -e
+#
+#CPUS_AVAILABLE=1
+#
+#case "$(uname -s)" in
+#Darwin)
+#    CPUS_AVAILABLE=$(sysctl -n machdep.cpu.core_count)
+#    ;;
+#Linux)
+#    CFS_QUOTA=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
+#    if [ $CFS_QUOTA -ge 100000 ]; then
+#    CPUS_AVAILABLE=$(expr ${CFS_QUOTA} / 100 / 1000)
+#    fi
+#    ;;
+#*)
+#    # Unsupported host OS. Must be Linux or Mac OS X.
+#    ;;
+#esac
+#
+#echo ${CPUS_AVAILABLE}
+
+
 #!/bin/bash
 
 set -e
 
 CPUS_AVAILABLE=1
 
-case "$(uname -s)" in
-Darwin)
-    CPUS_AVAILABLE=$(sysctl -n machdep.cpu.core_count)
-    ;;
-Linux)
+if [ "$(uname -s)" == "Linux" ]; then
     CFS_QUOTA=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
     if [ $CFS_QUOTA -ge 100000 ]; then
     CPUS_AVAILABLE=$(expr ${CFS_QUOTA} / 100 / 1000)
     fi
-    ;;
-*)
-    # Unsupported host OS. Must be Linux or Mac OS X.
-    ;;
-esac
+fi
 
 echo ${CPUS_AVAILABLE}
